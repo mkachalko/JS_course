d402bf4bafcbdba599bc8d664ef1bd28359922d1
Margarita Kachalko <margarita.kachalko@gmail.com>
2021-11-28 20:24:03 +0300

    lesson02 completed

--- script.js
+++ script.js
@@ -5,6 +5,23 @@ let rollback;
 let fullPrice;
 let adaptive;
 
-alert("Модальное окно с текстом");
+title = "JS course";
+screens = "Простые, Сложные, Интерактивные";
+screenPrice = 180;
+rollback = 20;
+fullPrice = 450;
+adaptive = true;
 
-console.log("Сообщение с текстом в консоли");
+console.log(typeof title);
+console.log(typeof fullPrice);
+console.log(typeof adaptive);
+
+console.log(screens.length);
+
+console.log("Стоимость верстки экранов" + " " + screenPrice + " " + "рублей");
+console.log("Стоимость разработки сайта" + " " + fullPrice + " " + "рублей");
+
+screens = screens.toLowerCase();
+console.log(screens.split(", "));
+
+console.log(fullPrice * (rollback / 100));
