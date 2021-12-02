c7e725951bdcb10135124872ce08bcde394fd97c
Margarita Kachalko <margarita.kachalko@gmail.com>
2021-12-02 11:12:04 +0300

    lesson05 completed

--- script.js
+++ script.js
@@ -1,19 +1,31 @@
 "use strict";
 
-let title = prompt("Как называется ваш проект?");
-let screens = prompt(
-  "Какие типы экранов нужно разработать?",
-  "Простые, Сложные, Интерактивные"
-);
-let screenPrice = +prompt("Сколько будет стоить данная работа", "12000");
+let title;
+let screens;
+let screenPrice;
 let rollback = 20;
-let adaptive = confirm("Нужен ли адаптив на сайте?");
-let service1 = prompt("Какой дополнительный тип услуги нужен?");
-let servicePrice1 = +prompt("Сколько это будет стоить?");
-let service2 = prompt("Какой дополнительный тип услуги нужен?");
-let servicePrice2 = +prompt("Сколько это будет стоить?");
-
+let adaptive;
+let service1;
+let servicePrice;
 let allServicePrices, fullPrice, servicePercentPrice, rollbackSum;
+
+const isNumber = function (num) {
+  return !isNaN(parseFloat(num)) && isFinite(num) && num !== null;
+};
+
+const asking = function () {
+  title = prompt("Как называется ваш проект?");
+  screens = prompt(
+    "Какие типы экранов нужно разработать?",
+    "Простые, Сложные, Интерактивные"
+  );
+  do {
+    screenPrice = prompt("Сколько будет стоить данная работа");
+  } while (!isNumber(screenPrice));
+  screenPrice = +screenPrice; // после проверки записываем в переменную число
+
+  adaptive = confirm("Нужен ли адаптив на сайте?");
+};
 
 const showTypeOf = function (variable) {
   console.log(variable, typeof variable);
@@ -32,8 +44,21 @@ const getRollbackMessage = function (price) {
   }
 };
 
-const getAllServicePrices = function (serv1, serv2) {
-  return serv1 + serv2;
+const getAllServicePrices = function () {
+  let sum = 0;
+
+  for (let i = 0; i < 2; i++) {
+    service1 = prompt("Какой дополнительный тип услуги нужен?");
+    servicePrice = prompt("Сколько это будет стоить?");
+
+    while (!isNumber(servicePrice)) {
+      servicePrice = prompt("Сколько это будет стоить?");
+    }
+    servicePrice = +servicePrice; // после проверки записываем в переменную число
+    sum += servicePrice;
+  }
+
+  return sum;
 };
 
 function getFullPrice(screen, service) {
@@ -49,7 +74,9 @@ function getServicePercentPrices(foo, rollb) {
   return foo - rollb;
 }
 
-allServicePrices = getAllServicePrices(servicePrice1, servicePrice2);
+asking();
+
+allServicePrices = getAllServicePrices();
 fullPrice = getFullPrice(screenPrice, allServicePrices);
 rollbackSum = fullPrice * (rollback / 100);
 servicePercentPrice = getServicePercentPrices(fullPrice, rollbackSum);
@@ -68,3 +95,7 @@ console.log(servicePercentPrice);
 // console.log(fullPrice);
 // console.log(title);
 // console.log(servicePercentPrice);
+// showTypeOf(allServicePrices);
+// showTypeOf(fullPrice);
+// showTypeOf(rollbackSum);
+// showTypeOf(servicePercentPrice);
